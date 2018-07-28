8488fe96a03f554f751e76e43d3f3c2a81e126bc
Jonathan Walker <jonathan.walker@ufl.edu>
2018-07-28 14:41:27 -0400

    Install Google Tag Manager

--- header.php
+++ header.php
@@ -4,6 +4,11 @@
 		<meta charset="<?php bloginfo('charset'); ?>">
 		<title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' : '; } ?><?php bloginfo('name'); ?></title>
 
+		<!-- Google Tag Manager -->
+		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+		})(window,document,'script','dataLayer','GTM-NSPS99N');</script>
+		<!-- End Google Tag Manager -->
 		<link href="//www.google-analytics.com" rel="dns-prefetch">
         <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
 
@@ -18,11 +23,15 @@
 
 	</head>
 	<body <?php body_class(); ?>>
+		<!-- Google Tag Manager (noscript) -->
+		<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NSPS99N"
+		height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+		<!-- End Google Tag Manager (noscript) -->
 		<header>
 			<nav>
 				<ul>
 					<li class="nav-logo"><img src="images/logo-icon.png" alt="logo" title="logo"></li>
-					<!-- Since php will inject an extra div around the actually menu links you will need to adjust CSS selectors accordingly. -->
+					<!-- Since php will inject an extra div around the actual menu links you will need to adjust CSS selectors accordingly. -->
 					<?php html5blank_nav(); ?>
 				</ul>
 			</nav>
