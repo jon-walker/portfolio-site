1013fd5a75dbe1ab29233384152fa2e13cfb110f
Jonathan Walker <jonathan.walker@ufl.edu>
2018-07-28 16:07:31 -0400

    Create js directory and load basic js files

--- footer.php
+++ footer.php
@@ -1,6 +1,7 @@
 		<?php wp_footer(); ?>
 
-		<!-- TODO: ADD GOOGLE ANALYTICS -->
-
+		<!-- SCRIPTS-->
+		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+		<script src="js/main.min.js"></script>
 	</body>
 </html>
