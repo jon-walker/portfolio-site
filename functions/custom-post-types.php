086ee1cd4db95066152a9db807ffce6e8da9b7e0
Jonathan Walker <jonathan.walker@ufl.edu>
2018-07-28 21:40:55 -0400

    Create Projects post type

--- functions/custom-post-types.php
+++ functions/custom-post-types.php
@@ -1,27 +1,27 @@
-<?php 
+<?php
 /*------------------------------------*\
 	Custom Post Types
 \*------------------------------------*/
 
 // Create 1 Custom Post type for a Demo, called HTML5-Blank
-function create_post_type_html5()
+function create_post_type_projects()
 {
-    /*
-    register_post_type('html5-blank', // Register Custom Post Type
+
+    register_post_type('jw_projects', // Register Custom Post Type
         array(
         'labels' => array(
-            'name' => __('HTML5 Blank Custom Post', 'html5blank'), // Rename these to suit
-            'singular_name' => __('HTML5 Blank Custom Post', 'html5blank'),
-            'add_new' => __('Add New', 'html5blank'),
-            'add_new_item' => __('Add New HTML5 Blank Custom Post', 'html5blank'),
-            'edit' => __('Edit', 'html5blank'),
-            'edit_item' => __('Edit HTML5 Blank Custom Post', 'html5blank'),
-            'new_item' => __('New HTML5 Blank Custom Post', 'html5blank'),
-            'view' => __('View HTML5 Blank Custom Post', 'html5blank'),
-            'view_item' => __('View HTML5 Blank Custom Post', 'html5blank'),
-            'search_items' => __('Search HTML5 Blank Custom Post', 'html5blank'),
-            'not_found' => __('No HTML5 Blank Custom Posts found', 'html5blank'),
-            'not_found_in_trash' => __('No HTML5 Blank Custom Posts found in Trash', 'html5blank')
+            'name' => __('Projects', 'jw_projects'), // Rename these to suit
+            'singular_name' => __('Project', 'jw_projects'),
+            'add_new' => __('Add New', 'jw_projects'),
+            'add_new_item' => __('Add New HTML5 Blank Custom Post', 'jw_projects'),
+            'edit' => __('Edit', 'jw_projects'),
+            'edit_item' => __('Edit HTML5 Blank Custom Post', 'jw_projects'),
+            'new_item' => __('New HTML5 Blank Custom Post', 'jw_projects'),
+            'view' => __('View HTML5 Blank Custom Post', 'jw_projects'),
+            'view_item' => __('View HTML5 Blank Custom Post', 'jw_projects'),
+            'search_items' => __('Search HTML5 Blank Custom Post', 'jw_projects'),
+            'not_found' => __('No HTML5 Blank Custom Posts found', 'jw_projects'),
+            'not_found_in_trash' => __('No HTML5 Blank Custom Posts found in Trash', 'jw_projects')
         ),
         'public' => true,
         'hierarchical' => true, // Allows your posts to behave like Hierarchy Pages
@@ -32,17 +32,17 @@ function create_post_type_html5()
             'excerpt',
             'thumbnail'
         ), // Go to Dashboard Custom HTML5 Blank post for supports
-        'menu_icon' => 'dashicons-format-status',   // http://www.kevinleary.net/wordpress-dashicons-list-custom-post-type-icons/
+        'menu_icon' => 'dashicons-star-empty',   // http://www.kevinleary.net/wordpress-dashicons-list-custom-post-type-icons/
         'can_export' => true // Allows export in Tools > Export
+        'menu_position' => 5 // Sets Placement in WP Admin to after posts
     ));
     register_taxonomy(
-        'type_of_staff',    // Taxonomy
-        'staff',             // Object Type
+        'jw_skills',    // Taxonomy
+        'jw_projects',  // Object Type
         array(
-            'label' => __( 'Type' ),
-            'rewrite' => array( 'slug' => 'type' ),
-            'hierarchical' => true, // Is this taxonomy hierarchical like categories or not hierarchical like tags.
+            'label' => __( 'Skills' ),
+            'rewrite' => array( 'slug' => 'Skill' ),
+            'hierarchical' => false, // Is this taxonomy hierarchical like categories or not hierarchical like tags.
         )
     );
-    */
 }
