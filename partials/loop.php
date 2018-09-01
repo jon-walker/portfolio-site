8b1d02250e1bed0f3b08bd46b2bd339193cb8d5e
Jonathan Walker <jonathan.walker@ufl.edu>
2018-09-01 18:23:19 -0400

    Remove author information from archive pages

--- partials/loop.php
+++ partials/loop.php
@@ -23,7 +23,6 @@
 				<?php the_date(); ?> <?php the_time(); ?>
 			</time>
 		</span>
-		<span class="author"><?php _e( 'Published by', 'html5blank' ); ?> <?php the_author_posts_link(); ?></span>
 		<span class="comments"><?php if (comments_open( get_the_ID() ) ) comments_popup_link( __( 'Leave your thoughts', 'html5blank' ), __( '1 Comment', 'html5blank' ), __( '% Comments', 'html5blank' )); ?></span>
 		<!-- /post details -->
 
