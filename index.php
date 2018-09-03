084e173005b0129653f9a8934720e27e370c1848
Jonathan Walker <jonathan.walker@ufl.edu>
2018-09-02 23:13:50 -0400

    Create basic index

--- index.php
+++ index.php
@@ -1,19 +1,11 @@
 <?php get_header(); ?>
+<h1><?php _e( 'Latest Posts', 'html5blank' ); ?></h1>
 
-	<main>
-		<!-- section -->
-		<section>
+<main>
+	<?php get_template_part('partials/loop'); ?>
+	<?php get_template_part('partials/pagination'); ?>
+</main>
 
-			<h1><?php _e( 'Latest Posts', 'html5blank' ); ?></h1>
-
-			<?php get_template_part('partials/loop'); ?>
-
-			<?php get_template_part('partials/pagination'); ?>
-
-		</section>
-		<!-- /section -->
-	</main>
-
-<?php get_sidebar(); ?>
+<?php dynamic_sidebar('blog-widgets'); ?>
 
 <?php get_footer(); ?>
