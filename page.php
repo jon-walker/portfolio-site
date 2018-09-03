e59b4484ab3de8847a8c044aa97471950711cec4
Jonathan Walker <jonathan.walker@ufl.edu>
2018-09-03 09:31:59 -0400

    Simplify default page markup

--- page.php
+++ page.php
@@ -1,45 +1,31 @@
 <?php get_header(); ?>
 
 	<main>
-		<!-- section -->
-		<section>
-
-			<h1><?php the_title(); ?></h1>
+		<h1><?php the_title(); ?></h1>
 
 		<?php if (have_posts()): while (have_posts()) : the_post(); ?>
 
-			<!-- article -->
-			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+		<!-- article -->
+		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-				<?php the_content(); ?>
+			<?php the_content(); ?>
 
-				<?php comments_template( '', true ); // Remove if you don't want comments ?>
-
-				<br class="clear">
-
-				<?php edit_post_link(); ?>
-
-			</article>
-			<!-- /article -->
+		</article>
+		<!-- /article -->
 
 		<?php endwhile; ?>
 
 		<?php else: ?>
 
-			<!-- article -->
-			<article>
+		<!-- article -->
+		<article>
 
-				<h2><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h2>
+			<h2><?php _e( 'Sorry, I seem to have misplaced some of my content. Have you seen it? It\'s about yay high and looks really awesome.', 'html5blank' ); ?></h2>
 
-			</article>
-			<!-- /article -->
+		</article>
+		<!-- /article -->
 
 		<?php endif; ?>
-
-		</section>
-		<!-- /section -->
 	</main>
-
-<?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
