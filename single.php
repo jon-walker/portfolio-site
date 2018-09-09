f846425e1d8b33f1cd17a0b280eaa21cca066fac
Jonathan Walker <jonathan.walker@ufl.edu>
2018-09-09 10:57:30 -0400

    Simplify blog markup

--- single.php
+++ single.php
@@ -1,8 +1,6 @@
 <?php get_header(); ?>
 
 	<main>
-	<!-- section -->
-	<section>
 
 	<?php if (have_posts()): while (have_posts()) : the_post(); ?>
 
@@ -10,38 +8,34 @@
 		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 			<!-- post thumbnail -->
-			<?php if ( has_post_thumbnail()) : // Check if Thumbnail exists ?>
-				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-					<?php the_post_thumbnail(); // Fullsize image for the single post ?>
-				</a>
-			<?php endif; ?>
+			<?php if ( has_post_thumbnail()) :               the_post_thumbnail(); endif; ?>
 			<!-- /post thumbnail -->
 
 			<!-- post title -->
 			<h1>
-				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+				<?php the_title(); ?>
 			</h1>
 			<!-- /post title -->
 
-			<!-- post details -->
-			<span class="date">
+			<!-- ul.blog_post-meta -->
+            <ul class="blog_post-meta">
+			<li class="date">
 				<time datetime="<?php the_time('Y-m-d'); ?> <?php the_time('H:i'); ?>">
-					<?php the_date(); ?> <?php the_time(); ?>
+					<?php the_date(); ?>
 				</time>
-			</span>
-			<span class="author"><?php _e( 'Published by', 'html5blank' ); ?> <?php the_author_posts_link(); ?></span>
-			<span class="comments"><?php if (comments_open( get_the_ID() ) ) comments_popup_link( __( 'Leave your thoughts', 'html5blank' ), __( '1 Comment', 'html5blank' ), __( '% Comments', 'html5blank' )); ?></span>
-			<!-- /post details -->
+			</li>
+			<li class="author"><?php _e( 'Written by', 'html5blank' ); ?> <?php the_author(); ?></li>
+			<li class="comments"><?php if (comments_open( get_the_ID() ) ) comments_popup_link( __( 'Leave your thoughts', 'html5blank' ), __( '1 Comment', 'html5blank' ), __( '% Comments', 'html5blank' )); ?></li>
+            </ul>
+			<!-- /ul.blog_post-meta -->
 
 			<?php the_content(); // Dynamic Content ?>
 
 			<?php the_tags( __( 'Tags: ', 'html5blank' ), ', ', '<br>'); // Separated by commas with a line break at the end ?>
 
-			<p><?php _e( 'Categorised in: ', 'html5blank' ); the_category(', '); // Separated by commas ?></p>
+			<p><?php _e( 'Categories: ', 'html5blank' ); the_category(', '); // Separated by commas ?></p>
 
-			<p><?php _e( 'This post was written by ', 'html5blank' ); the_author(); ?></p>
-
-			<?php edit_post_link(); // Always handy to have Edit Post Links available ?>
+			<p><?php _e( 'Written by ', 'html5blank' ); the_author(); ?></p>
 
 			<?php comments_template(); ?>
 
@@ -62,8 +56,6 @@
 
 	<?php endif; ?>
 
-	</section>
-	<!-- /section -->
 	</main>
 
 <?php get_sidebar(); ?>
