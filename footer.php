ad9317bfc3fd754fce11f72f9a6a064ce3f52b11
Jonathan Walker <jonathan.walker@ufl.edu>
2018-09-16 15:04:23 -0400

    Add privacy policy to footer

--- footer.php
+++ footer.php
@@ -1,4 +1,9 @@
-		<?php wp_footer(); ?>
+        <footer>
+            <p><small>&copy; Jonathan Walker <?= get_the_date('Y'); ?></small></p>
+            <p><small><a href="<?= get_home_url(); ?>/privacy-policy/" target="_blank">Privacy Policy</a></small></p>
+        </footer>
+
+        <?php wp_footer(); ?>
 
 		<!-- SCRIPTS-->
 		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
