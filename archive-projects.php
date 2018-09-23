8f76082bdbf4d068af4832e529ddbaf433ee7850
Jonathan Walker <jonathan.walker@ufl.edu>
2018-09-23 11:19:29 -0400

    Change headline wording for project index

--- archive-projects.php
+++ archive-projects.php
@@ -2,13 +2,11 @@
     <header class="blog_header">
         <img class="blog_header_logo" src="<?= get_template_directory_uri(); ?>/img/projects-logo.png" alt="See My Recent Design Projects">
     </header>
-    <h1><?php _e( 'Latest Projects', 'html5blank' ); ?></h1>
+    <h1><?php _e( 'Check Out My Past Projects', 'html5blank' ); ?></h1>
 
     <main>
     	<?php get_template_part('partials/loop'); ?>
     	<?php get_template_part('partials/pagination'); ?>
     </main>
-
-<?php dynamic_sidebar('blog-widgets'); ?>
 
 <?php get_footer(); ?>
